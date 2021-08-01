f7293b2294ee46efaefe55d39330db79b89227da
Gregory Salinas <asalinas720@gmail.com>
2021-08-01 13:41:19 -0700

    Update SEO and compose script

--- README.md
+++ README.md
@@ -117,7 +117,7 @@ layout: PostLayout
 
 ### Compose
 
-Run `node ./scripts/compose.js` to bootstrap a new post.
+Run `npm run compose` to bootstrap a new post.
 
 Follow the interactive prompt to generate a post with pre-filled front matter.
 
--- components/SEO.js
+++ components/SEO.js
@@ -9,13 +9,16 @@ export const PageSeo = ({ title, description }) => {
       <title>{`${title}`}</title>
       <meta name="robots" content="follow, index" />
       <meta name="author" content={siteMetadata.author} />
-      <meta name="description" content={description} />
+      <meta name="title" property="og:title" content={title} />
+      <meta name="description" property="og:description" content={description} />
+      <meta
+        name="image"
+        property="og:image"
+        content={`${siteMetadata.siteUrl}${siteMetadata.socialBanner}`}
+      />
       <meta property="og:url" content={`${siteMetadata.siteUrl}${router.asPath}`} />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content={siteMetadata.title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:image" content={`${siteMetadata.siteUrl}${siteMetadata.socialBanner}`} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={siteMetadata.twitter} />
       <meta name="twitter:title" content={title} />
